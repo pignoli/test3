8dd2c8ef9e3842a915486d96d0d88a89858fda80
pignoli <wmishima@gmail.com>
2022-04-17 11:07:42 +0900

    2-4-1

---
+++ album/admin.py
@@ -0,0 +1,7 @@
+from atexit import register
+from django.contrib import admin
+from .models import Tag, Photo
+
+admin.site.register(Tag)
+admin.site.register(Photo)
+
---
+++ album/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import Settings, settings
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns=[
+    path('',views.index),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
